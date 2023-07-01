f0a376bfa0720b4a6a8fe05e878aad6b61a87008
san2025chez <segundousuario@example.com>
2023-07-01 20:26:23 -0300

    add-bd

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import express from "express";
 import morgan from "morgan";
 import path from "path";
-import cors from 'cors'
-import {ACCESS_TOKEN, PORT} from '../config.js'
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { config } from 'dotenv';
+
 import paymentRoutes from "./routes/payment.routes.js";
-
-
+dotenv.config();
+config();
 const app = express();
 app.use(express.json());
 app.use(cors()); // Habilitar CORS para todas las solicitudes
 app.use(morgan("dev"));
+// Configura la conexión a la base de datos PostgreSQL
+
 
 app.use(paymentRoutes);
 
 //app.use(express.static(path.resolve("src/public")));
 
-app.listen(PORT);
-console.log("Server on port", PORT);
+app.listen(process.env.PORT_SERVER);
+console.log("Server on port", process.env.PORT_SERVER);
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   createOrder,
   receiveWebhook,
+  saveData
 } from "../controllers/payment.controller.js";
 
 const router = Router();
@@ -9,6 +10,8 @@ const router = Router();
 router.post("/create-order", createOrder);
 
 router.post("/webhook", receiveWebhook);
+
+router.post('/guardar-datos', saveData)
 
 router.get("/success", (req, res) => res.send("Success"));
 
